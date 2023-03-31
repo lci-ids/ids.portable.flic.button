2d76d70f52336cd1ffd86f06f448375633c63296
Chris Price <cprice1@lci1.com>
2023-03-31 13:25:04 -0400

    Expose FlicManager init through Platform Initializer

--- src/ids.portable.flic.button/Platforms/Android/PlatformInitializer.cs
+++ src/ids.portable.flic.button/Platforms/Android/PlatformInitializer.cs
@@ -1,14 +1,19 @@
-﻿using IDS.Portable.Flic.Button.Platforms.Shared;
+﻿using Android.Content;
+using Android.OS;
+using IDS.Portable.Flic.Button.Platforms.Shared;
+using IO.Flic.Flic2libandroid;
 
 namespace IDS.Portable.Flic.Button.Platforms.Android
 {
     public static class PlatformInitializer
     {
         public static int NotificationIcon;
-        public static void Init()
+        public static void Init(Context context)
         {
             ServiceCollection.RegisterSingleton<IFlicButtonManager, NativeFlicButtonManager>();
             ServiceCollection.Init();
+            
+            Flic2Manager.Init(context, new Handler());
         }
     }
 }
